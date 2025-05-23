0e9520275d2374337fa629c3dd35d93b5d667655
Miguel Flandez <immfpnvs2@gmail.com>
2025-05-23 18:03:57 +0800

    created a parent for tracker component instead of calling on app

---
+++ src/pages/TrackingPage.jsx
@@ -0,0 +1,12 @@
+import Tracker from "@/components/Tracker"
+
+export default function TrackingPage(){
+
+
+    return(
+        <div className='p-20'>
+            <Tracker/>
+        </div>
+
+    )
+}
